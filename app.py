3cdf2ff1fab7f535da424d4ce7e3704f2a9881da
Israelshecktar <Iamshecktar1996@gmail.com>
2024-03-20 13:14:13 +0100

    fixed encodings

--- app.py
+++ app.py
@@ -206,15 +206,23 @@ def attendance():
 def generate(userImage):
     IMAGE_FILES = []
     filename = []
-    imageDir = 'seemeehere/faces'
+
+    # Updated image directory path
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    imageDir = os.path.join(BASE_DIR, 'seemeehere', 'faces')
 
     if userImage:
         img_path = os.path.join(imageDir, userImage)
-        #print(img_path)
-        img_path = face_recognition.load_image_file(img_path)
-        IMAGE_FILES.append(img_path)
-        filename.append(userImage.split(".", 1)[0])
 
+        # Pre-check for image existence
+        if not os.path.exists(img_path):
+            print(f"File not found: {img_path}")
+            return  # Or handle more gracefully depending on your application needs
+
+        # Load and append image if exists
+        img = face_recognition.load_image_file(img_path)
+        IMAGE_FILES.append(img)
+        filename.append(userImage.split(".", 1)[0])
 
     def encoding_img(IMAGE_FILES):
         encodeList = []
@@ -224,9 +232,8 @@ def generate(userImage):
             encodeList.append(encode)
         return encodeList
 
-
-    def addAttendence(name):
-       with open('attendence.csv', 'r+') as f:
+    def addAttendance(name):
+        with open('attendance.csv', 'r+') as f:
             mypeople_list = f.readlines()
             dateList = []
             now = datetime.now()
@@ -235,70 +242,59 @@ def generate(userImage):
                 entry = line.split(',')
                 dateList.append(entry[1])
             if datestring not in dateList:
-                timestring = now.strftime('%H:%M:%S')
                 f.writelines(f'\n{name},{datestring}')
 
-    encodeListknown = encoding_img(IMAGE_FILES)
-
+    encodeListKnown = encoding_img(IMAGE_FILES)
     cap = cv2.VideoCapture(0)
 
     while True:
         success, img = cap.read()
         imgc = cv2.resize(img, (0, 0), None, 0.25, 0.25)
-        # converting image to RGB from BGR
         imgc = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-        fasescurrent = face_recognition.face_locations(imgc)
-        encode_fasescurrent = face_recognition.face_encodings(imgc, fasescurrent)
+        facesCurrent = face_recognition.face_locations(imgc)
+        encode_facesCurrent = face_recognition.face_encodings(imgc, facesCurrent)
 
-        # faceloc- one by one it grab one face location from fasescurrent
-        # than encodeFace grab encoding from encode_fasescurrent
-        # we want them all in same loop so we are using zip
-        for encodeFace, faceloc in zip(encode_fasescurrent, fasescurrent):
-            matches_face = face_recognition.compare_faces(encodeListknown, encodeFace)
-            face_distence = face_recognition.face_distance(encodeListknown, encodeFace)
-            # print(face_distence)
-            # finding minimum distence index that will return best match
-            matchindex = np.argmin(face_distence)
+        for encodeFace, faceLoc in zip(encode_facesCurrent, facesCurrent):
+            matches_face = face_recognition.compare_faces(encodeListKnown, encodeFace)
+            face_distance = face_recognition.face_distance(encodeListKnown, encodeFace)
+            matchIndex = np.argmin(face_distance)
 
-            if matches_face[matchindex]:
-                name = filename[matchindex].upper()
+            if matches_face[matchIndex]:
+                name = filename[matchIndex].upper()
                 putText = 'Captured'
-                y1, x2, y2, x1 = faceloc
-                # multiply locations by 4 because we above we reduced our webcam input image by 0.25
-                # y1,x2,y2,x1 = y1*4,x2*4,y2*4,x1*4
+                y1, x2, y2, x1 = faceLoc
+
                 cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 2)
-                cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (255, 0, 0), 2, cv2.FILLED)
+                cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (255, 0, 0), cv2.FILLED)
                 cv2.putText(img, putText, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
-                addAttendence(name)  # taking name for attendence function above
+                addAttendance(name)  # taking name for attendance function above
 
         frame = cv2.imencode('.jpg', img)[1].tobytes()
         yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
         key = cv2.waitKey(20)
         if key == 27:
-           cap.release()
-           cv2.destroyAllWindows()
-           break
+            cap.release()
+            cv2.destroyAllWindows()
+            break
 
 @app.route('/take_attendance/<userid>')
 def take_attendance(userid):
-    # Directly use the userid from the URL parameter instead of trying to get it from request.args
     print(f"Attempting to take attendance for user ID: {userid}")
 
     cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-    # Ensure the query correctly uses the placeholder syntax for parameters
     cursor.execute('SELECT * FROM user WHERE id = %s', (userid, ))
     user = cursor.fetchone()
 
     if user and user['picture']:
-        print(f"User found with picture: {user['picture']}")  # Log user picture
+        print(f"User found with picture: {user['picture']}")
         try:
             return Response(generate(user['picture']),
-                                mimetype='multipart/x-mixed-replace; boundary=frame')
+                            mimetype='multipart/x-mixed-replace; boundary=frame')
         except Exception as e:
             print(f"An error occurred: {e}")
             traceback.print_exc()
-            return 'Error during face recognition process', 500  # Provide a generic error message for the client
+            return 'Error during face recognition process', 500
     else:
         print("No user picture available or user not found")
         return 'No user picture available or user not found', 404
