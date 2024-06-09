dfdb935bcb56f6d5269509c6984313e49c793dbd
Israelshecktar <iamshecktar1996@gmail.com>
2024-06-09 11:37:42 +0000

    improved sql connections

--- instance/config.py
+++ instance/config.py
@@ -1,7 +1,7 @@
 SECRET_KEY = 'ab2123445'
 UPLOAD_FOLDER = 'faces'
-MYSQL_HOST = '127.0.0.1'
+MYSQL_HOST = 'codespaces-b72e9f'
 MYSQL_PORT = 3306
 MYSQL_USER = 'root'
-MYSQL_PASSWORD = 'abolanle12@'
+MYSQL_PASSWORD = 'Abolanle12@'
 MYSQL_DB = 'attendance-system-python'
