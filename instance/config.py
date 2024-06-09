7bc15ab7964fc5050db2f08d45d0685939755111
Israelshecktar <iamshecktar1996@gmail.com>
2024-06-09 11:44:25 +0000

    removed instance

--- instance/config.py
+++
@@ -1,7 +0,0 @@
-SECRET_KEY = 'ab2123445'
-UPLOAD_FOLDER = 'faces'
-MYSQL_HOST = 'codespaces-b72e9f'
-MYSQL_PORT = 3306
-MYSQL_USER = 'root'
-MYSQL_PASSWORD = 'Abolanle12@'
-MYSQL_DB = 'attendance-system-python'
